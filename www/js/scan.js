a3e3b7ec1b710611768de2bb5e772ae89e791d21
ocpanda <tma590g2t1r3@gmail.com>
2016-08-04 11:50:05 +0800

    出現字串問題

--- www/js/scan.js
+++ www/js/scan.js
@@ -3,36 +3,46 @@ var scanapp = {
 	goScan: function(){
 		this.bindEvents();
 	},
+	/**
+	 * [bindEvents description]
+	 * define event listener
+	 * @return {[type]} [description]
+	 * no return
+	 */
 	bindEvents: function(){
+		//start scan listener button DOM id=scanBtn
 		var scanBtn = document.getElementById("scanBtn");
 		scanBtn.addEventListener('click', this.deviceScan, false);
+		//stop scan listener button DOM id=stopScanBtn
+		var stopScanBtn = document.getElementById("stopScanBtn");
+		stopScanBtn.addEventListener('click', this.deviceStopScan, false);
 	},
 
 	deviceScan: function(){
-		var scanSeconds = 5; //scan 10 seconds
-
-		
+		console.log("scanning!");
 		ble.startScan([], function(device){
 			console.log("here is scan device!");
 			//document.body.removeChild(document.getElementById("error"));
 			var obj = JSON.parse(JSON.stringify(device));
-			console.log(obj);
-			//var obj = JSON.parse(device);
-			//console.log(obj.id);
+			console.log(obj);	
 			var dataBody = document.getElementById("scanData");
 			var dataTr = document.createElement("tr");
 			var dataId = document.createElement("th");
 			var dataName = document.createElement("th");
 			var dataUUID = document.createElement("th");
-			var dataIdText  = document.createTextNode(1);
+			var dataRSSI = document.createElement("th");
+			var dataIdText  = document.createTextNode((dataNum+1));
 			var dataNameText = document.createTextNode(obj.name.toString());
 			var dataUUIDText = document.createTextNode(obj.id.toString());
+			var dataRSSIText = document.createElement(obj.rssi.toString());
 			dataId.appendChild(dataIdText);
 			dataName.appendChild(dataNameText);
 			dataUUID.appendChild(dataUUIDText);
+			dataRSSI.appendChild(dataRSSIText);
 			dataTr.appendChild(dataId);
 			dataTr.appendChild(dataName);
 			dataTr.appendChild(dataUUID);
+			dataTr.appendChild(dataRSSI);
 			dataBody.appendChild(dataTr);
 		},function(reason){
 			console.log("doesn't scan devices!");
@@ -44,13 +54,20 @@ var scanapp = {
 			document.body.appendChild(errorBody);
 		});
 
-		setTimeout(ble.stopScan, scanSeconds*1000,
+		/*setTimeout(ble.stopScan, scanSeconds*1000,
 			function(){
 				console.log("Scan complete");
 			},
 			function(){
 				console.log("stopScan faild");
-			});
+			});*/
+	},
+
+	deviceStopScan: function(){
+		console.log("stop scan!");
+		ble.stopScan(
+			function(){console.log("stop scan!");},
+			function(){console.log("stop scan faild!");});
 	}
 }
 scanapp.goScan();
