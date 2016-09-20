57f0eb8028ecfa185423dd10a9be398f283d5a71
ocpanda <tma590g2t1r3@gmail.com>
2016-09-20 19:38:29 +0800

    工具列改至下方 長案說明欄完成但只有進入點會運作

--- platforms/android/assets/www/js/changePage.js
+++ platforms/android/assets/www/js/changePage.js
@@ -3,27 +3,24 @@
  *
  * 變數:
  * pages 定義所有頁面
+ * pagesNum 紀錄目前頁面
  */
-
 var pages = ["index.html", "search.html", "group.html", "day.html", "message.html", "scan.html"];
 
-//$(document).on("pageinit", "index.html", function(){
-	//向右滑動
-	
-	var pagesNum = pages.length;
-	$(document).bind("swiperight", function(){
-		pagesNum = ((pagesNum - 1) < 0)? pages.length-1: pagesNum-1;
-		$.mobile.changePage(pages[pagesNum],{
-			"transition": "slide",
-			"reverse": true
-		});
+//向右滑動
+var pagesNum = pages.length;
+$(document).bind("swiperight", function(){
+	pagesNum = ((pagesNum - 1) < 0)? pages.length-1: pagesNum-1;
+	$.mobile.changePage(pages[pagesNum],{
+		"transition": "flip",
+		"reverse": true
 	});
+});
 
-	//向左滑動
-	$(document).bind("swipeleft", function(){
-		pagesNum = ++pagesNum % pages.length;
-		$.mobile.changePage(pages[pagesNum],{
-			"transition": "slide"
-		});
+//向左滑動
+$(document).bind("swipeleft", function(){
+	pagesNum = ++pagesNum % pages.length;
+	$.mobile.changePage(pages[pagesNum],{
+		"transition": "flip"
 	});
-//});
+});
--- platforms/browser/www/web_api/jquery/gk-loader/tools/minify.sh
+++
@@ -1,5 +0,0 @@
-uglifyjs ../gk-loader.js -c -m > ../gk-loader.min.js
-uglifyjs ../html.js -c -m -r keys,gk > ../html.min.js
-uglifyjs ../html-ie8.js -c -m -r keys,gk > ../html-ie8.min.js
-uglifyjs ../wdgt.js -c -m > ../wdgt.min.js
-uglifyjs ../lib/simplehtmlparser.js -c -m > ../lib/simplehtmlparser.min.js
