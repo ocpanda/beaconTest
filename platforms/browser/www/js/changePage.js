d31499304d41232923439353925301db3dd8d851
ocpanda <tma590g2t1r3@gmail.com>
2016-09-22 17:12:57 +0800

    將ajax加載模式關閉(目前只有超連結有效)

--- platforms/browser/www/js/changePage.js
+++ platforms/browser/www/js/changePage.js
@@ -7,11 +7,13 @@
  */
 var pages = ["index.html", "search.html", "group.html", "day.html", "message.html", "scan.html"];
 
+$.mobile.ignoreContentEnabled=true;
 //向右滑動
 var pagesNum = pages.length;
 $(document).bind("swiperight", function(){
 	pagesNum = ((pagesNum - 1) < 0)? pages.length-1: pagesNum-1;
 	$.mobile.changePage(pages[pagesNum],{
+		"reloadPage": true,
 		"transition": "flip",
 		"reverse": true
 	});
@@ -21,6 +23,7 @@ $(document).bind("swiperight", function(){
 $(document).bind("swipeleft", function(){
 	pagesNum = ++pagesNum % pages.length;
 	$.mobile.changePage(pages[pagesNum],{
+		"reloadPage": true,
 		"transition": "flip"
 	});
 });
