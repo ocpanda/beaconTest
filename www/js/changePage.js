a272a1671716228a46afb8009e25b6ef4a31e5de
ocpanda <tma590g2t1r3@gmail.com>
2016-09-28 21:41:03 +0800

    將主要畫面放到index內做頁面切換   各功能內畫面利用ajax來注入功能區塊

--- www/js/changePage.js
+++ www/js/changePage.js
@@ -6,14 +6,26 @@
  * pagesNum 紀錄目前頁面
  */
 var pages = ["index.html", "search.html", "group.html", "day.html", "message.html", "scan.html"];
+var footNavBars = ["home", "search", "group", "day", "message", "setting"];
+
+var nowPage;
+var nowPages;
+for(var i=0; i<6; i++){
+	nowPage = location.href.split("/");
+	nowPages = nowPage.indexOf(pages[i]);
+	
+	if(nowPages >= 0)
+		break;
+}
+nowPages = pages.indexOf(nowPage[nowPages]);
 
 $.mobile.ignoreContentEnabled=true;
 //向右滑動
-var pagesNum = pages.length;
+var pagesNum = nowPage;
 $(document).bind("swiperight", function(){
 	pagesNum = ((pagesNum - 1) < 0)? pages.length-1: pagesNum-1;
-	$.mobile.changePage(pages[pagesNum],{
-		"reloadPage": true,
+	$.mobile.changePage("#"+footNavBars[pagesNum],{
+		"rel": "external",
 		"transition": "flip",
 		"reverse": true
 	});
@@ -22,8 +34,26 @@ $(document).bind("swiperight", function(){
 //向左滑動
 $(document).bind("swipeleft", function(){
 	pagesNum = ++pagesNum % pages.length;
-	$.mobile.changePage(pages[pagesNum],{
-		"reloadPage": true,
+	$.mobile.changePage("#"+footNavBars[pagesNum],{
+		"rel": "external",
 		"transition": "flip"
 	});
 });
+
+
+/**
+ * 底下工具列渲染
+ *
+ * 變數:
+ * footNavBars 工具名稱
+ */
+
+/*
+$(document).on("pageshow", "#"+footNavBars[nowPages], function(){
+	alert("wegfawrgaerg");
+	for(var i=0; i<5; i++){
+		$("#"+footNavBars[nowPages]+"footNavBar").append("<li><a href=#"+footNavBars[nowPages]
+			+" id="+footNavBars[nowPages]+"Img  data-ajax='false'><img style='width:90%;' src='image/"
+			+footNavBars[nowPages]+".png' is='image'></a></li>");
+	}
+});*/
